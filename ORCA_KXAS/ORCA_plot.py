93b900c36f47a8a63138d5e2cc50f34f5cf53325
Sebastian Aguiar <sbas.acastrillon@gmail.com>
2024-11-28 20:57:34 -0500

    ORCA Plot

---
+++ ORCA_KXAS/ORCA_plot.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+def plot(file, plot = False, plot_bar = False, name = None, x_name = None,x_lim = None, y_name = None, width = 10):
+    """
+    This function, `plot`, is designed to plot data from a given file using either a line plot or a bar 
+    plot, or both, based on the provided arguments.
+    
+    Returns:
+    - name: The label name for the plot.
+    - x: The x-axis data.
+    - y_normalized: The normalized y-axis data.
+    """
+
+    x, y = file[:, 0], file[:, 1]
+    y_normalized = (y - np.min(y)) / (np.max(y) - np.min(y))
+
+    if plot:
+        plt.figure(figsize=(10, 6))
+        plt.plot(x, y_normalized,linewidth=width ,label=name, alpha=0.6)
+        plt.xlabel(x_name)
+        plt.xlim(x_lim[0], x_lim[1])
+        plt.ylabel(y_name)
+        plt.legend()
+        plt.show()
+
+    if plot_bar:
+        plt.figure(figsize=(10, 6))
+        plt.bar(x, y_normalized, width=width, label=name, alpha=0.6)
+        plt.xlabel(x_name)
+        plt.xlim(x_lim[0], x_lim[1])
+        plt.ylabel(y_name)
+        plt.legend()
+        plt.show()
+    return name, x, y_normalized
