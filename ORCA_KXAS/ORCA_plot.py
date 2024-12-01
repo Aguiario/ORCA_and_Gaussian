0f7ef98a2eac409298396d51a421e1cd397377f6
Sebastian Aguiar <sbas.acastrillon@gmail.com>
2024-11-30 20:25:02 -0500

    XANES Analysis

--- ORCA_KXAS/ORCA_plot.py
+++ ORCA_KXAS/ORCA_plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-def plot(file, plot = False, plot_bar = False, name = None, x_name = None,x_lim = None, y_name = None, width = 10):
+def plot(file, plot = False, plot_bar = False, name = None, x_name = None,x_lim = None, y_name = None, width = 10, normalized = False, **kwargs):
     """
     This function, `plot`, is designed to plot data from a given file using either a line plot or a bar 
     plot, or both, based on the provided arguments.
@@ -10,15 +10,17 @@ def plot(file, plot = False, plot_bar = False, name = None, x_name = None,x_lim 
     Returns:
     - name: The label name for the plot.
     - x: The x-axis data.
-    - y_normalized: The normalized y-axis data.
+    - y: The  y-axis data.
     """
 
     x, y = file[:, 0], file[:, 1]
-    y_normalized = (y - np.min(y)) / (np.max(y) - np.min(y))
+
+    if normalized:
+        y = (y - np.min(y)) / (np.max(y) - np.min(y))
 
     if plot:
         plt.figure(figsize=(10, 6))
-        plt.plot(x, y_normalized,linewidth=width ,label=name, alpha=0.6)
+        plt.plot(x, y,linewidth=width ,label=name, alpha=0.6)
         plt.xlabel(x_name)
         plt.xlim(x_lim[0], x_lim[1])
         plt.ylabel(y_name)
@@ -27,10 +29,10 @@ def plot(file, plot = False, plot_bar = False, name = None, x_name = None,x_lim 
 
     if plot_bar:
         plt.figure(figsize=(10, 6))
-        plt.bar(x, y_normalized, width=width, label=name, alpha=0.6)
+        plt.bar(x, y, width=width, label=name, alpha=0.6)
         plt.xlabel(x_name)
         plt.xlim(x_lim[0], x_lim[1])
         plt.ylabel(y_name)
         plt.legend()
         plt.show()
-    return name, x, y_normalized
+    return name, x, y
