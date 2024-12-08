5b8b1dd6879ab2072a39bebdf4c2276d498762b0
Sebastian Aguiar <sbas.acastrillon@gmail.com>
2024-12-08 11:30:05 -0500

    XANES 0 to 10 eV

--- ORCA_KXAS/ORCA_plot.py
+++ ORCA_KXAS/ORCA_plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-def plot(file, plot = False, plot_bar = False, name = None, x_name = None,x_lim = None, y_name = None, width = 10, normalized = False, **kwargs):
+def plot(file, plot = False, plot_bar = False, name = None, x_name = None,x_lim = None, y_name = None, width = 10, normalized = False):
     """
     This function, `plot`, is designed to plot data from a given file using either a line plot or a bar 
     plot, or both, based on the provided arguments.
@@ -31,7 +31,8 @@ def plot(file, plot = False, plot_bar = False, name = None, x_name = None,x_lim 
         plt.figure(figsize=(10, 6))
         plt.bar(x, y, width=width, label=name, alpha=0.6)
         plt.xlabel(x_name)
-        plt.xlim(x_lim[0], x_lim[1])
+        if x_lim:
+            plt.xlim(x_lim[0], x_lim[1])
         plt.ylabel(y_name)
         plt.legend()
         plt.show()
